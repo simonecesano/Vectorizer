251bf38a6ecf167f7c6ce5ac1e4e7c47091072bd
Simone Cesano <simonecesano@libero.it>
2020-12-23 16:18:26 +0100

    Added gpu component

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var app = new Vue({
     el: '#app',
     mounted: function(){
-	console.log('foo');
     },
     router: (new VueRouter({
 	routes: [
-	    { path: '/',         redirect: '/hist' },
+	    { path: '/',      redirect: '/hist' },
 	    { path: '/hist',  component: httpVueLoader('components/hist.vue') },
+	    { path: '/gpu',   component: httpVueLoader('components/gpu.vue') },
 	]})),
     data: {
 	screen: {}
